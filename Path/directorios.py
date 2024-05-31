8d6ae127d9d1b49a0c7cd1267873530a59be355f
Nesgc <nestorgarciac11@gmail.com>
2024-05-30 22:08:32 -0600

    directoriosproject

--- Path/directorios.py
+++ Path/directorios.py
@@ -2,27 +2,60 @@ from pathlib import Path
 import os
 
 rutabase=os.path.abspath(os.getcwd())
+recetasfolder= Path(rutabase,'Path','Recetas')
 print('Bienvenido usuario, la ruta de acceso al directorio se encuentra en: ')
-print(Path(rutabase,'Recetas'))
-# base= Path.home()
-# guia = Path(base,"Barcelona", "SagradraFamilia.txt")
-# guia2=guia.with_name("La_pedrera.txt")
-# print(base)
-# print(guia)
-# print(guia2)
-# #Para obtener la carpeta parent anterior
-# print
+print(recetasfolder)
 
-# guia = Path(Path.home(),'Desktop','PythonProjects','pythonlearn','Path',"Europa")
+print('Esta es la cantidad de recetas que tienes: ')
+def recetastxt():
+    # for txt in Path(recetasfolder).glob("**/*.txt"):
+    #     print(txt)
+    contador= 0
+    for txt in Path(recetasfolder).glob("**/*.txt"):
+        
+        contador+=1
+    print(contador)
 
-# for txt in Path(guia).glob("**/*.txt"):
-#     print(txt)
+recetastxt()
 
-  
-# system('cls')
+def optionone():
+    recetascarnes= Path(os.path.abspath(os.getcwd()),'Path','Recetas','Carnes')
 
-# def abrir_leer(ruta_archivo):
-#     with open(ruta_archivo, 'r') as archivo:
-#         le=archivo.read()
-#         return(le)
-       
+    category=input('Que categoria eliges? Carne, Ensaladas, Pastas, Postres:')
+    if category == 'Carne':
+        for txt in Path(recetascarnes).glob("**/*.txt"):
+            print(txt.name)
+
+        Recetacarneelegida = input('Elija una receta de carne: ')
+        receta_path = recetascarnes / Recetacarneelegida
+        if Recetacarneelegida == 'Entrecot al Malbec.txt':
+            archivo=open(receta_path,'r')
+            contenido=archivo.read()
+            print(contenido)
+    elif category == 'Ensaladas':
+        print('elegiste 2')
+    elif category == 'Pastas':
+        print('elegiste 3')
+    elif category == 'Postres':
+        print('elegiste 4')
+    else:
+        print('Selecciona una opcion valida')
+        optionone()
+
+def choose():
+    optionchosen=input('Elige una opcion 1,2,3 o 4')
+    if optionchosen == '1':
+        optionone()
+
+    elif optionchosen == '2':
+        print('elegiste 2')
+    elif optionchosen == '3':
+        print('elegiste 3')
+    elif optionchosen == '4':
+        print('elegiste 4')
+    else:
+        choose()
+
+choose()
+
+
